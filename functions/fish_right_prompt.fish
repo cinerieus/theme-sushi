9be1fbb325ab8ce7d36084aa74752d8a005bafb1
cinerieus <cinereus@protonmail.com>
2022-02-10 01:17:23 +0000

    Update fish_right_prompt.fish

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -9,7 +9,7 @@ function fish_right_prompt
 		if git::is_repo
 			set root_folder (command git rev-parse --show-toplevel 2>/dev/null)
 			set parent_root_folder (dirname $root_folder)
-			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||"]
+			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
 		end
 	end
 
