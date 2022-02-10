5d8d1944f73bf5ee05d7d8f18e2074eec96cf357
cinerieus <cinereus@protonmail.com>
2022-02-10 01:14:13 +0000

    Update fish_right_prompt.fish

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -9,24 +9,24 @@ function fish_right_prompt
 		if git::is_repo
 			set root_folder (command git rev-parse --show-toplevel 2>/dev/null)
 			set parent_root_folder (dirname $root_folder)
-			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+			set cwd (echo $PWD | sed -e "s|$parent_root_folder/||"]
 		end
 	end
 
 	command -sq kubectl; and k8s::current_context >/dev/null 2>/dev/null; and begin
 		set -l k8s_namespace (k8s::current_namespace)
 		if test -z "$k8s_namespace"
-			printf (magenta)"("(dim)(k8s::current_context)(magenta)") "(off)
+			printf (magenta)"["(dim)(k8s::current_context)(magenta)"] "(off)
 		else
-			printf (magenta)"("(dim)(k8s::current_context)"/$k8s_namespace"(magenta)") "(off)
+			printf (magenta)"["(dim)(k8s::current_context)"/$k8s_namespace"(magenta)"] "(off)
 		end
 	end
 
 	if terraform::workspace
 		set terraform_workspace_name (command cat .terraform/environment)
-		printf (magenta)"("(dim)$terraform_workspace_name(magenta)") "(off)
+		printf (magenta)"["(dim)$terraform_workspace_name(magenta)"] "(off)
 	end
 
-	printf (magenta)"("(dim)$cwd(magenta)") "(off)
+	printf (magenta)"["(dim)$cwd(magenta)"] "(off)
 	printf (dim)(date +%H(magenta):(dim)%M(magenta):(dim)%S)(off)
 end
