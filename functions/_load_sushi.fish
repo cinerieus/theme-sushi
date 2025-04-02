f4f8f43a0f9766bd0384f1f9c83ee2a496ff5f74
cinerieus <cinereus@protonmail.com>
2025-04-02 14:27:17 +0100

    Update _load_sushi.fish

--- functions/_load_sushi.fish
+++ functions/_load_sushi.fish
@@ -1,87 +1,38 @@
 # Colors
 function orange
-    set_color -o ee5819
+    set_color -o f5a97f
 end
 
 function yellow
-    set_color -o b58900
+    set_color -o eed49f
 end
 
 function red
-    set_color -o d30102
+    set_color -o ed8796
 end
 
 function cyan
-    set_color -o 44a7ab
+    set_color -o 7dc4e4
 end
 
 function magenta
-    set_color -o 994df0
+    set_color -o b7bdf8
 end
 
 function blue
-    set_color -o 4043f5
+    set_color -o 8aadf4
 end
 
 function white
-    set_color -o fdf6e3
+    set_color -o cad3f5
 end
 
 function dim
-    set_color -o 6e6e6e
+    set_color -o 5b6078
 end
 
 function off
     set_color -o normal
-end
-
-# Git
-function git::is_repo
-	test -d .git; or command git rev-parse --git-dir >/dev/null 2>&1
-end
-
-function git::branch_name
-	git::is_repo; and begin
-		command git symbolic-ref --short HEAD 2>/dev/null;
-		or command git show-ref --head -s --abbrev | head -n1 2>/dev/null
-	end
-end
-
-function git::is_dirty
-	git::is_repo; and not command git diff --no-ext-diff --quiet --exit-code
-end
-
-function git::is_staged
-	git::is_repo; and begin
-		not command git diff --cached --no-ext-diff --quiet --exit-code
-	end
-end
-
-function git::is_stashed
-	git::is_repo; and begin
-		command git rev-parse --verify --quiet refs/stash >/dev/null
-	end
-end
-
-# Kubernetes
-function k8s::current_context
-    command kubectl config current-context
-end
-
-function k8s::current_namespace
-    command kubectl config view --minify -o jsonpath='{.contexts[0].context.namespace}'
-end
-
-# Terraform
-# Test whether this is a terraform directory by finding .tf files
-function terraform::directory
-	command find . -name '*.tf' >/dev/null 2>&1 -maxdepth 0
-end
-
-function terraform::workspace
-	terraform::directory; and begin
-		test -e .terraform/environment
-	end
 end
 
 function _load_sushi
