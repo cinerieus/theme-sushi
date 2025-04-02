115fa14cda57fe86cf729a3b56b9b1403d98c616
cinerieus <cinereus@protonmail.com>
2025-04-02 14:48:54 +0100

    Update _load_sushi.fish

--- functions/_load_sushi.fish
+++ functions/_load_sushi.fish
@@ -38,26 +38,3 @@ end
 function _load_sushi
 	# do nothing
 end
-
-set -x color_normal cad3f5
-set -x color_command 8aadf4
-set -x color_param f0c6c6
-set -x color_keyword ed8796
-set -x color_quote a6da95
-set -x color_redirection f5bde6
-set -x color_end f5a97f
-set -x color_comment 8087a2
-set -x color_error ed8796
-set -x color_gray 6e738d
-set -x color_selection --background=363a4f
-set -x color_search_match --background=363a4f
-set -x color_option a6da95
-set -x color_operator f5bde6
-set -x color_escape ee99a0
-set -x color_autosuggestion 6e738d
-set -x color_cancel ed8796
-set -x color_host_remote a6da95
-set -x color_status ed8796
-set -x pager_color_progress 6e738d
-set -x pager_color_prefix f5bde6
-set -x pager_color_description 6e738d
